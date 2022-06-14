5f5b9e0381d2a48c4552d242af3552b870b4ba8f
vijjini19 <107198946+vijjini19@users.noreply.github.com>
2022-06-14 13:07:06 +0530

    Add files via upload

---
+++ variables_04day.py
@@ -0,0 +1,40 @@
+'''
+Case Sensitive:The Difference Between Uppercase and Lowercase.
+The Variables is used to store the values.
+We can reuse the variables in python.
+A variable can start with alphabet.
+A variable cannot start with numeric but it can be in the middle and ending.
+A variable can start with uppercase.
+A variable should not have space between them.'''
+
+
+
+# a=5
+# b=8
+# print(a+b)
+# a=9
+# print(a)
+# ABC=87
+# print(ABC)
+# 7ab=78
+# print(7ab)
+
+# _hj=647566778998
+# print(_hj)
+
+# print('python'.isidentifier())
+# print('_ajb'.isidentifier())
+# print('8hkk'.isidentifier())
+# print(' gh'.isidentifier())
+# print('_ uu'.isidentifier())
+# print('hj_ '.isidentifier())
+# print('g78'.isidentifier())
+
+
+# a=7
+# b=9
+# print(a)
+# print(b)
+# a,b=b,a
+# print(a)
+# print(b)
